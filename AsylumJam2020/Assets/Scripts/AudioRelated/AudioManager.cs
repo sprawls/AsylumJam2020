c5cb97bf2f8cc93250f3eeef69bf6890f762f96f
MSI\etirb <etirb@hotmail.com>
2020-12-26 17:50:02 -0400

    Ambiant sound added + audio mixer hierarchy

---
+++ AsylumJam2020/Assets/Scripts/AudioRelated/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip Ambiance;
+
+    public AudioClip Track1;
+    public AudioClip Track2;
+    public AudioClip Track3;
+    public AudioClip Track4;
+    public AudioClip Track5;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
