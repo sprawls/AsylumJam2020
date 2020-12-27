e9fdd45220cf0d9311d37b0cab0697919f316ada
MSI\etirb <etirb@hotmail.com>
2020-12-26 20:15:53 -0400

    AudioManager debut + son ambiance et routing mix sfx player

--- AsylumJam2020/Assets/Scripts/AudioRelated/AudioManager.cs
+++ AsylumJam2020/Assets/Scripts/AudioRelated/AudioManager.cs
@@ -5,19 +5,64 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    public AudioSource audioSource;
+    private const float MuteValue = -80.00f;
 
-    public AudioClip Ambiance;
+    [Header("Ambiance")]
+    public AudioMixerGroup AmbianceMixer;
+    public float AmbianceVolume;
+    public bool AmbianceMute;
 
+    [Header("SFX")]
+    public AudioMixerGroup SFXMixer;
+    public float SFXVolume;
+    public bool SFXMute;
+
+    [Header("Music")]
+    public AudioMixerGroup MusicMixer;
+    public float MusicVolume;
+    public bool MusicMute;
+    public AudioMixerGroup[] MusicTracks;
 
     void Start()
     {
-        
+        SetMixerVolumeParam();
     }
 
     void Update()
     {
-        
+        SetMixerVolumeParam();
     }
 
+    public void SetMixerVolumeParam()
+    {
+        //Ambiance
+        if (AmbianceMute)
+        {
+            AmbianceMixer.audioMixer.SetFloat("AmbiantVolumeParam", MuteValue);
+        }
+        else
+        {
+            AmbianceMixer.audioMixer.SetFloat("AmbiantVolumeParam", AmbianceVolume);
+        }
+
+        //SFX
+        if (SFXMute)
+        {
+            AmbianceMixer.audioMixer.SetFloat("SFXVolumeParam", MuteValue);
+        }
+        else
+        {
+            SFXMixer.audioMixer.SetFloat("SFXVolumeParam", SFXVolume);
+        }
+
+        //Music
+        if (MusicMute)
+        {
+            AmbianceMixer.audioMixer.SetFloat("MusicVolumeParam", MuteValue);
+        }
+        else
+        {
+            MusicMixer.audioMixer.SetFloat("MusicVolumeParam", MusicVolume);
+        }      
+    }
 }
