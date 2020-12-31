0b3475e6238612453d4951b085c8ff54590e55de
sprawls <alex3521@hotmail.fr>
2020-12-31 12:35:17 -0500

    Ending Working, redid refs in scene

--- AsylumJam2020/Assets/Scripts/Volume/TeleportPlayer.cs
+++ AsylumJam2020/Assets/Scripts/Volume/TeleportPlayer.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class TeleportPlayer : MonoBehaviour
 {
     [SerializeField] private Vector3 _offset;
+    [SerializeField] private int _timesItCanTrigger = -1;
+
+    private int _timesTrigger = 0;
 
     void OnTriggerEnter(Collider coll) {
-        if(coll.tag == "Player") {
+        if(coll.tag == "Player" && (_timesItCanTrigger<0 || _timesTrigger< _timesItCanTrigger)) {
             coll.transform.position += _offset;
+            ++_timesTrigger;
         }
     }
 
