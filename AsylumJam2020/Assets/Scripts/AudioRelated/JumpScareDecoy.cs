c28640dce5e38969edc22d6cbba4eb6b17652354
MSI\etirb <etirb@hotmail.com>
2020-12-31 22:13:22 -0400

    Jumpscare prefab

---
+++ AsylumJam2020/Assets/Scripts/AudioRelated/JumpScareDecoy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScareDecoy : MonoBehaviour
+{
+    public AudioSource audio;
+    private string _playerAudioTag = "PlayerAudio";
+    public SphereCollider sphereCollider;
+
+    public AudioClip [] OtherSounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio.maxDistance = sphereCollider.radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == _playerAudioTag)
+        {
+            Debug.Log("JumpScare");
+            PlayRandomSounds();
+        }
+    }
+
+    //RandomPlayer
+    public void PlayRandomSounds()
+    {
+        int randNumb = UnityEngine.Random.Range(0, OtherSounds.Length);
+        audio.PlayOneShot(OtherSounds[randNumb]);
+    }
+
+}
